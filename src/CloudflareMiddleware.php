fd897a8217e2a12a387a076298fcf099638c4414
Your Name <you@example.com>
2018-01-10 21:45:41 +0200

    code rearrange

--- src/CloudflareMiddleware.php
+++ src/CloudflareMiddleware.php
@@ -25,97 +25,14 @@ class CloudflareMiddleware
     const REFRESH_EXPRESSION = '/8;URL=(\/cdn-cgi\/l\/chk_jschl\?pass=[0-9]+\.[0-9]+-.*)/';
 
     /** @var callable */
-    private $nextHandler;
+    protected $nextHandler;
 
     /**
      * @param callable $nextHandler Next handler to invoke.
      */
-    public function __construct($nextHandler)
+    public function __construct(callable $nextHandler)
     {
         $this->nextHandler = $nextHandler;
-    }
-
-    /**
-     * @param RequestInterface $request
-     * @param array $options
-     * @return ResponseInterface
-     */
-    public function __invoke(RequestInterface $request, array $options = [])
-    {
-        $next = $this->nextHandler;
-
-        return $next($request, $options)
-            ->then(function (ResponseInterface $response) use ($request, $options) {
-                return $this->checkResponse($request, $options, $response);
-            });
-    }
-
-    /**
-     * @param RequestInterface $request
-     * @param array $options
-     * @param ResponseInterface $response
-     * @return ResponseInterface
-     * @throws \Exception
-     */
-    protected function checkResponse(RequestInterface $request, array $options = [], ResponseInterface $response)
-    {
-        if (!$this->needVerification($response)) return $response;
-
-        if (empty($options['cookies']) || ! $options['cookies']) {
-            throw new Exception('you have to use cookies');
-        }
-
-        if (empty($options['allow_redirects']) || ! $options['allow_redirects']) {
-            throw new Exception('you have to use the allow_redirects options');
-        }
-
-        return $this($this->modifyRequest($request, $response), $options);
-    }
-
-    /**
-     * @param ResponseInterface $response
-     * @return bool
-     */
-    protected function needVerification(ResponseInterface $response)
-    {
-        return $response->getStatusCode() === static::WAIT_RESPONSE_CODE
-            && $response->getHeaderLine('Server') === static::SERVER_NAME;
-    }
-
-    /**
-     * @param RequestInterface $request
-     * @param ResponseInterface $response
-     * @return RequestInterface
-     */
-    protected function modifyRequest(RequestInterface $request, ResponseInterface $response)
-    {
-        sleep(8);
-
-        return modify_request($request, [
-            'uri' => UriResolver::resolve(
-                $request->getUri(),
-                $this->getRefreshUri($response)
-            ),
-            'body' => '',
-            'method' => 'GET',
-            'set_headers' => [
-                'Referer' => $request->getUri()->withUserInfo('', '')
-            ]
-        ]);
-    }
-
-    /**
-     * @param ResponseInterface $response
-     * @return Uri
-     * @throws \Exception
-     */
-    protected function getRefreshUri(ResponseInterface $response)
-    {
-        if (preg_match(static::REFRESH_EXPRESSION, $response->getHeaderLine('Refresh'), $matches)) {
-            return new Uri($matches[1]);
-        }
-
-        throw new Exception('Can not seem to parse the refresh header');
     }
 
     /**
@@ -126,5 +43,96 @@ class CloudflareMiddleware
         return function ($handler) {
             return new static($handler);
         };
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param array $options
+     * @return \Psr\Http\Message\RequestInterface
+     */
+    public function __invoke(RequestInterface $request, array $options = [])
+    {
+        $next = $this->nextHandler;
+
+        return $next($request, $options)
+            ->then(
+                function (ResponseInterface $response) use ($request, $options) {
+                    return $this->checkResponse($request, $options, $response);
+                }
+            );
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param array $options
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @return \Psr\Http\Message\RequestInterface|\Psr\Http\Message\ResponseInterface
+     * @throws \Exception
+     */
+    protected function checkResponse(RequestInterface $request, array $options = [], ResponseInterface $response)
+    {
+        if (!$this->needVerification($response)) {
+            return $response;
+        }
+
+        if (empty($options['cookies'])) {
+            throw new Exception('you have to use cookies');
+        }
+
+        if (empty($options['allow_redirects'])) {
+            throw new Exception('you have to use the allow_redirects options');
+        }
+
+        return $this($this->modifyRequest($request, $response), $options);
+    }
+
+    /**
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @return bool
+     */
+    protected function needVerification(ResponseInterface $response)
+    {
+        return $response->getStatusCode() === static::WAIT_RESPONSE_CODE
+            && $response->getHeaderLine('Server') === static::SERVER_NAME;
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @return \Psr\Http\Message\RequestInterface
+     * @throws \Exception
+     */
+    protected function modifyRequest(RequestInterface $request, ResponseInterface $response)
+    {
+        sleep(8);
+
+        return modify_request(
+            $request,
+            [
+                'uri' => UriResolver::resolve(
+                    $request->getUri(),
+                    $this->getRefreshUri($response)
+                ),
+                'body' => '',
+                'method' => 'GET',
+                'set_headers' => [
+                    'Referer' => $request->getUri()->withUserInfo('', ''),
+                ],
+            ]
+        );
+    }
+
+    /**
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @return \GuzzleHttp\Psr7\Uri
+     * @throws \Exception
+     */
+    protected function getRefreshUri(ResponseInterface $response)
+    {
+        if (preg_match(static::REFRESH_EXPRESSION, $response->getHeaderLine('Refresh'), $matches)) {
+            return new Uri($matches[1]);
+        }
+
+        throw new Exception('Can not seem to parse the refresh header');
     }
 }
